da9cf50638a18a0491f71007b2986f9a8b417d47
Wojciech Sznapka <wojciech.sznapka@cherrygroup.com>
2014-10-26 23:26:10 +0100

    Getting statuses and changing state

--- pihome.py
+++ pihome.py
@@ -10,6 +10,28 @@ def switch_list():
     switch = SwitchService()
     data = switch.get_list()
 
-    return jsonify({'count': len(data), 'total': len(data), '_embedded': data})
+    response = jsonify({'count': len(data), 'total': len(data), '_embedded': data})
+    response.headers['Access-Control-Allow-Origin'] = '*'
+
+    return response
+
+@app.route('/switch/<key>', methods = ['OPTIONS'])
+def switch_options(key):
+    response = jsonify({})
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Access-Control-Allow-Methods'] = 'GET,PATCH'
+    response.headers['Access-Control-Allow-Headers'] = 'Content-Type'
+
+    return response
+@app.route('/switch/<key>', methods = ['PATCH'])
+def switch_toggle(key):
+    switch = SwitchService()
+    data = switch.toggle(key, request.values.get('state'))
+
+    response = jsonify(data)
+    response.headers['Access-Control-Allow-Origin'] = '*'
+
+    return response
+
 
 app.run(host='0.0.0.0', port=8999, debug=True)
--- switch.py
+++ switch.py
@@ -9,37 +9,59 @@ class SwitchService:
     def __init__(self):
         self.config = yaml.load(file(os.path.dirname(os.path.realpath(__file__)) + '/config.yml'))['switch']
 
+
     def get_list(self):
         switches = {}
-        for key,device in self.config['devices'].iteritems():
-            switches[key] = {
-                'key': key,
-                'name': device['name'],
-                'status': self.get_status(device),
-            }
+        for key in self.config['devices']:
+            switches[key] = self.__get_switch(key)
 
         return switches
 
-    def get_status(self, device):
-        return self.__get_switch(device['type'], device).get_status()
 
-    def __get_switch(self, type, params):
-        if 'ethernet' == type:
+    def get_state(self, device):
+        return self.__get_switch_driver(device).get_state()
+
+
+    def toggle(self, key, new_state):
+        device = self.config['devices'][key]
+        self.__get_switch_driver(device).set_state(new_state)
+
+        return self.__get_switch(key)
+
+
+
+    def __get_switch(self, key):
+        device = self.config['devices'][key]
+
+        return {
+            'key': key,
+            'name': device['name'],
+            'state': self.get_state(device)
+        }
+
+
+    def __get_switch_driver(self, params):
+        if 'ethernet' == params['type']:
             return EthernetSwitch(self.config['ethernet']['address'], params['address'])
-        elif 'raspberry' == type:
+        elif 'raspberry' == params['type']:
             return RaspberrySwitch(params)
 
 class EthernetSwitch:
     def __init__(self, url, address):
         self.address = address
-        self.url = url
+        self.url = 'http://' + url
 
-    def get_status(self):
-        statusXml = urllib2.urlopen('http://' + self.url + '/status.xml').read()
+    def get_state(self):
+        statusXml = urllib2.urlopen(self.url + '/status.xml').read()
         xml = parseString(statusXml)
 
         return xml.getElementsByTagName('led' + str(self.address))[0].firstChild.nodeValue
 
+    def set_state(self, new_state):
+        current_state = self.get_state()
+        if current_state != new_state:
+            urllib2.urlopen(self.url + '/leds.cgi?led=' + str(self.address)).read()
+
 class RaspberrySwitch:
-    def get_status(self):
+    def get_state(self):
         return -1
