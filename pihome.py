7aed6c9ba07b8f052053357940689335a24a939a
Wojciech Sznapka <wojciech.sznapka@cherrygroup.com>
2014-10-26 23:27:02 +0100

    Removed CORS

--- pihome.py
+++ pihome.py
@@ -10,28 +10,13 @@ def switch_list():
     switch = SwitchService()
     data = switch.get_list()
 
-    response = jsonify({'count': len(data), 'total': len(data), '_embedded': data})
-    response.headers['Access-Control-Allow-Origin'] = '*'
+    return jsonify({'count': len(data), 'total': len(data), '_embedded': data})
 
-    return response
-
-@app.route('/switch/<key>', methods = ['OPTIONS'])
-def switch_options(key):
-    response = jsonify({})
-    response.headers['Access-Control-Allow-Origin'] = '*'
-    response.headers['Access-Control-Allow-Methods'] = 'GET,PATCH'
-    response.headers['Access-Control-Allow-Headers'] = 'Content-Type'
-
-    return response
 @app.route('/switch/<key>', methods = ['PATCH'])
 def switch_toggle(key):
     switch = SwitchService()
     data = switch.toggle(key, request.values.get('state'))
 
-    response = jsonify(data)
-    response.headers['Access-Control-Allow-Origin'] = '*'
-
-    return response
-
+    return jsonify(data)
 
 app.run(host='0.0.0.0', port=8999, debug=True)
