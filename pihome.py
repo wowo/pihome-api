ffdaa7d50d5f1815339ff0ebf95fc5e15c4355db
Wojciech Sznapka <wojciech.sznapka@cherrygroup.com>
2014-11-01 21:30:25 +0100

    Added CORS in Flask

--- pihome.py
+++ pihome.py
@@ -12,11 +12,25 @@ def switch_list():
 
     return jsonify({'count': len(data), 'total': len(data), '_embedded': data})
 
+
 @app.route('/switch/<key>', methods = ['PATCH'])
 def switch_toggle(key):
     switch = SwitchService()
     data = switch.toggle(key, request.values.get('state'))
 
     return jsonify(data)
+
+
+@app.after_request
+def add_cors(resp):
+    """ Ensure all responses have the CORS headers. This ensures any failures are also accessible
+        by the client. """
+    resp.headers['Access-Control-Allow-Origin'] = request.headers.get('Origin','*')
+    resp.headers['Access-Control-Allow-Credentials'] = 'true'
+    resp.headers['Access-Control-Allow-Methods'] = 'POST, OPTIONS, GET, PATCH'
+    resp.headers['Access-Control-Allow-Headers'] = request.headers.get('Access-Control-Request-Headers', 'Authorization')
+
+    return resp
+
 
 app.run(host='0.0.0.0', port=8999, debug=True)
