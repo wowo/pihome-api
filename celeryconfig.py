155888d072ba28eef87674a20665d2ce7eb86d81
Wojciech Sznapka <wojciech@sznapka.pl>
2017-07-03 23:16:46 +0200

    Changes in celeryconfig (Redis)

--- celeryconfig.py
+++ celeryconfig.py
@@ -2,5 +2,5 @@
 
 CELERY_RESULT_SERIALIZER='json'
 
-BROKER_URL='redis://localhost:6379/0'
-RESULT_BACKEND = 'redis://localhost:6379/0'
+BROKER_URL='redis://127.0.0.1:6379/0'
+RESULT_BACKEND = 'redis://127.0.0.1:6379/0'
